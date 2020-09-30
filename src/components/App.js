04d43d640654d7aecbba3896e505ed1a6e233762
anupamroy8 <anupamroy8@gmail.com>
2020-09-30 11:54:34 +0530

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div className="ui container">
+      <h1>App</h1>
+    </div>
+  );
+};
+
+export default App;
