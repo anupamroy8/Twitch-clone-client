419f0bba0925124e74885a4ee06f34c125a0ac5d
anupamroy8 <anupamroy8@gmail.com>
2020-10-03 20:17:02 +0530

    types and actions added for stream

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
+import { connect } from "react-redux";
+import { createStream } from "../../actions";
 
 class StreamCreate extends React.Component {
   renderError = ({ error, touched }) => {
@@ -19,9 +21,10 @@ class StreamCreate extends React.Component {
     );
   };
 
-  onSubmit(formValues) {
+  onSubmit = (formValues) => {
     console.log(formValues);
-  }
+    this.props.createStream(formValues);
+  };
 
   render() {
     return (
@@ -53,7 +56,9 @@ const validate = (formValues) => {
   return errors;
 };
 
-export default reduxForm({
-  form: "stram create",
+const formWrapped = reduxForm({
+  form: "stream create",
   validate,
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
